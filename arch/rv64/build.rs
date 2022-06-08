e22128de8bd7ca2a8bd5c0640053bda15e5e8b12
alqou91 <alqou91@gmail.com>
2022-06-08 23:58:49 +0900

    use opensbi (fw_jump.bin)

--- arch/rv64/build.rs
+++ arch/rv64/build.rs
@@ -4,8 +4,12 @@ use cc::Build;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+	println!("cargo:rerun-if-changed=src/trap.S");
+	println!("cargo:rerun-if-changed=src/start.S");
+
     let mut build = Build::new();
 	build.file("src/trap.S").compile("trap");
 	build.file("src/start.S").compile("start");
+
     Ok(())
 }
