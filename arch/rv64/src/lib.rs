9b246d83ef1f6eafb9d4a930261417ba6dd12c6c
alqou91 <alqou91@gmail.com>
2022-06-15 22:48:28 +0900

    mv asm to rs

--- arch/rv64/src/lib.rs
+++ arch/rv64/src/lib.rs
@@ -1,10 +1,44 @@
 #![no_std]
+#![feature(naked_functions)]
+#![feature(asm_sym)]
 
+use core::arch::asm;
 use csr::{Mode, Stvec};
 
+extern "C" {
+	static _stack: usize;
+}
+
+fn trap_handler() {
+}
+
 pub fn init_irq() {
-    Stvec::read()
-        .mode(Mode::Direct)
-        .address(0x9908)
-        .write();
+	Stvec::read()
+		.mode(Mode::Direct)
+		.address(_trap_handler as usize)
+		.write();
+}
+
+#[naked]
+pub extern "C" fn _trap_handler() {
+	unsafe {
+		asm!(
+			"j {handler}",
+			 "sret",
+			 handler = sym trap_handler,
+			 options(noreturn));
+	}
+}
+
+#[naked]
+#[link_section = ".start"]
+#[export_name = "_start"]
+pub extern "C" fn _start() {
+	unsafe {
+		asm!(
+			"la sp, {stack}",
+			"j main",
+			stack = sym _stack,
+			options(noreturn));
+	}
 }
